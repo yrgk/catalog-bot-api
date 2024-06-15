8df25963cb727354a9df2d12246dd946c6eeedff
yrgk <yarik090908@gmail.com>
2024-06-16 00:53:58 +0300

    fixed bug in fetching all catalog items (without refactoring)

--- database/utils.go
+++ database/utils.go
@@ -1,7 +1,7 @@
 package database
 
 import (
-	"catalog-bot-api/structs"
+	// "catalog-bot-api/structs"
 
 	"gorm.io/gorm"
 )
@@ -14,13 +14,18 @@ func Migrate(db *gorm.DB) {
 	)
 }
 
-func GetAllItems(db *gorm.DB, catalogID int) ([]structs.CatalogItemReponse, error) {
-	var catalogItems []structs.CatalogItemReponse
+func GetAllItems(db *gorm.DB, catalogID int) ([]CatalogItem, error) {
+	// var catalogItems []structs.CatalogItemReponse
+	var catalogItems []CatalogItem
 	// err := db.Model(&CatalogItem{}).Find(&catalogItems)
 	// err := db.Where("catalog_id = ?", catalogID).Find(&structs.CatalogItemReponse{})
-	err := db.Where("catalog_id = ?", catalogID).Find(&structs.CatalogItemReponse{})
-	if err != nil {
-		return nil, err.Error
+	result := db.Where("catalog_id = ?", catalogID).Find(&catalogItems)
+	// err := db.Find(&catalogItems)
+	// if err != nil {
+	// 	return nil, err.Error
+	// }
+	if result.Error != nil {
+		return nil, result.Error
 	}
 
 	return catalogItems, nil
@@ -28,10 +33,11 @@ func GetAllItems(db *gorm.DB, catalogID int) ([]structs.CatalogItemReponse, erro
 
 func GetAllCatalogs(db *gorm.DB, shopID int) ([]Catalog, error) {
 	var catalogs []Catalog
-	err := db.Where("shop_id = ?", shopID).Find(&catalogs)
-	if err != nil {
-		return nil, err.Error
-	}
+	// err := db.Where("shop_id = ?", shopID).Find(&catalogs)
+	db.Where("shop_id = ?", shopID).Find(&catalogs)
+	// if err != nil {
+	// 	return nil, err.Error
+	// }
 
 	return catalogs, nil
 }
